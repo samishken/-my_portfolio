631ba0e19eb919f8441fd76132db25b71d306d52
SAMUEL HAILE <samishken@gmail.com>
2018-04-22 17:08:05 -0400

    portfolio updated

---
+++ index.js
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Samuel Haile</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+            <h1>Welcome to Samuel Haile's page </h1>
+                <img src="https://sdtimes.com/wp-content/uploads/2016/12/1202.sdt-hour-of-code.png" alt="Sam" />
+                <ul>
+                  <li><a href="#">About Me</a></li>
+                  <li><a href="#">Projects</a></li>
+                  <li><a href="www.twitter.com/samishken">Social Media</a></li>
+                </ul>
+    </header>
+    <article>
+        <h2>Express, Node.Js</h2>
+            <p>Learning about Express and Node.Js.</p>
+            <button>Read more</button>
+    </article>
+    <article>
+        <h2>HTML, CSS, Javascript</h2>
+            <p>Finished learinging about HTML, CSS and Javascript.</p>
+            <button>Read more</button>
+    <article>
+        <h2>Moved to Washington, DC</h2>
+            <p>I just made the most important decision of my life.  Moving to the Capital city.</p>
+            <button>Read more</button>
+    </article>
+    <article>
+        <h2>Process of finding An apartment in Washington, DC</h2>
+            <p>I found this process to be worst than looking for a job.  My process started by calling a friend.</p>
+            <button>Read more</button>
+    </article>
+            
+    <footer>  
+        <p class="message">Created by: <span class="footer">Samishken</span></p>
+    </footer>
+
+</body>
+</html>
+
+
